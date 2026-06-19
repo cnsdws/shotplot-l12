b2331ecb398d4cccbb9e2776030608ca18d1cc94
Dennis Stevens <cnsdws@yahoo.com>
2026-06-19 17:34:37 -0600

    Replace Flash target with HTML5 canvas shot plotter

--- resources/views/displayfirestring.blade.php
+++ resources/views/displayfirestring.blade.php
@@ -1,4 +1,5 @@
 @extends('_firestringmaster')
+
 @section('title')
 <title>Display Firestring</title>
 @stop
@@ -7,115 +8,131 @@
 <br>
 <li><a href="/indexfirestring/{{ $firestring->match_id }}" class="navbar-brand">Back to Firestrings</a></li>
 
-	
-	@foreach($errors->all() as $message) 
-    <div class='error'>{{ $message }}</div>
-	@endforeach
+<br><br>
 
- 	<br>
-    <br>
-    <!-- Main page section for stock edit -->
-        <h3>Display a String of Fire</h3>
-    
-        <div class = "row">
-	    	<div class = "col-md-4"> 
-	    		<table class="table table-striped">
-		    		<tbody>
-		    			<h4> Firestring # {{$firestring->fire_string_number}}</h4>
-			    		<tr>
-			    			<td>Distance</td>
-			    			<td>{{ $firestring->distance }}</td>
-						</tr>
-						<tr>
-			    			<td>Target Number</td>
-			    			<td>{{ $firestring->target }}</td>
-						</tr>
-						<tr>
-			    			<td>Relay</td>
-			    			<td>{{ $firestring->relay }}</td>
-						</tr>
-						<tr>
-			    			<td>Light Direction</td>
-			    			<td>{{ $firestring->lightdirection }}</td>
-						</tr>
-						<tr>
-			    			<td>Wind Direction</td>
-			    			<td>{{ $firestring->winddirection }}</td>
-						</tr>
-						<tr>
-			    			<td>Wind Speed</td>
-			    			<td>{{ $firestring->windspeed }}</td>
-						</tr>
-						<tr>
-			    			<td>Elevation</td>
-			    			<td>{{ $firestring->elevation }}</td>
-						</tr>
-						<tr>
-			    			<td>Windage</td>
-			    			<td>{{ $firestring->windage }}</td>
-						</tr>
-					</tbody>
-				</table>	
-           </div>
-           <div class = "col-md-2"> 
-	    		<table class="table table-striped">
-		    		<tbody>
-		    			<h4> Shot and Score</h4>
-			    		<tr>
-			    			<td>Shot 1</td>
-			    			<td>{{$firestring->shot1value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 2</td>
-			    			<td>{{$firestring->shot2value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 3</td>
-			    			<td>{{$firestring->shot3value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 4</td>
-			    			<td>{{$firestring->shot4value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 5</td>
-			    			<td>{{$firestring->shot5value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 6</td>
-			    			<td>{{$firestring->shot6value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 7</td>
-			    			<td>{{$firestring->shot7value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 8</td>
-			    			<td>{{$firestring->shot8value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 9</td>
-			    			<td>{{$firestring->shot9value}}</td>
-			    		</tr>
-			    		<tr>
-			    			<td>Shot 10</td>
-			    			<td>{{$firestring->shot10value}}</td>
-			    		</tr>
+<h3>Display a String of Fire</h3>
 
-			    	</tbody>
-			    </table>
-			</div>
-			<div class = "col-md-4">
-				<h4> Plot Your Shots!</h4>
-				
-				<div id="flashContent">
-					<object width="550" height="550" data ="/200Yard.swf" align="middle"></object>
-				</div>
-			</div>
-				
-				
-      	</div>
-                     
+<div class="row">
+    <div class="col-md-4">
+        <table class="table table-striped">
+            <tbody>
+                <h4>Firestring #{{ $firestring->fire_string_number }}</h4>
+                <tr><td>Distance</td><td>{{ $firestring->distance }}</td></tr>
+                <tr><td>Target Number</td><td>{{ $firestring->target }}</td></tr>
+                <tr><td>Relay</td><td>{{ $firestring->relay }}</td></tr>
+                <tr><td>Light Direction</td><td>{{ $firestring->lightdirection }}</td></tr>
+                <tr><td>Wind Direction</td><td>{{ $firestring->winddirection }}</td></tr>
+                <tr><td>Wind Speed</td><td>{{ $firestring->windspeed }}</td></tr>
+                <tr><td>Elevation</td><td>{{ $firestring->elevation }}</td></tr>
+                <tr><td>Windage</td><td>{{ $firestring->windage }}</td></tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="col-md-2">
+        <table class="table table-striped">
+            <tbody>
+                <h4>Shot and Score</h4>
+                @for ($i = 1; $i <= 10; $i++)
+                    <tr>
+                        <td>Shot {{ $i }}</td>
+                        <td>{{ $firestring->{'shot'.$i.'value'} }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
+
+    <div class="col-md-6">
+        <h4>Shot Plot</h4>
+        <canvas id="targetCanvas" width="550" height="550" style="border:1px solid #ccc; max-width:100%;"></canvas>
+        <p class="text-muted">HTML5 target plotter replacing the original Flash target.</p>
+    </div>
+</div>
+
+<script>
+(function () {
+    const canvas = document.getElementById('targetCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const center = 275;
+    const maxRadius = 250;
+
+    const shots = [
+        @for ($i = 1; $i <= 10; $i++)
+            {
+                n: {{ $i }},
+                score: @json($firestring->{'shot'.$i.'value'}),
+                x: @json($firestring->{'shot'.$i.'x'}),
+                y: @json($firestring->{'shot'.$i.'y'})
+            }{{ $i < 10 ? ',' : '' }}
+        @endfor
+    ];
+
+    function drawTarget() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#f9f9f9';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 10; i >= 1; i--) {
+            const r = maxRadius * i / 10;
+
+            ctx.beginPath();
+            ctx.arc(center, center, r, 0, Math.PI * 2);
+            ctx.fillStyle = i <= 4 ? '#222' : '#fff';
+            ctx.fill();
+            ctx.strokeStyle = '#333';
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(center - maxRadius, center);
+        ctx.lineTo(center + maxRadius, center);
+        ctx.moveTo(center, center - maxRadius);
+        ctx.lineTo(center, center + maxRadius);
+        ctx.strokeStyle = '#999';
+        ctx.stroke();
+
+        ctx.fillStyle = '#000';
+        ctx.font = '13px Arial';
+        ctx.fillText('200 Yard Target', 12, 22);
+    }
+
+    function drawShots() {
+        shots.forEach(function (shot) {
+            if (shot.x === null || shot.y === null || shot.x === '' || shot.y === '') {
+                return;
+            }
+
+            const x = Number(shot.x);
+            const y = Number(shot.y);
+
+            if (Number.isNaN(x) || Number.isNaN(y)) {
+                return;
+            }
+
+            const isInBlack = Math.hypot(shot.x - center, shot.y - center) <= (maxRadius * 4 / 10);
+
+            ctx.beginPath();
+            ctx.arc(shot.x, shot.y, 9, 0, Math.PI * 2);
+            ctx.fillStyle = isInBlack ? '#ffffff' : '#d9534f';
+            ctx.fill();
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.fillStyle = isInBlack ? '#000000' : '#ffffff';
+            ctx.font = 'bold 13px Arial';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText(key, shot.x, shot.y);
+        });
+    }
+
+    drawTarget();
+    drawShots();
+})();
+</script>
 @stop
-
-
--- resources/views/editfirestring.blade.php
+++ resources/views/editfirestring.blade.php
@@ -1,120 +1,209 @@
 @extends('_firestringmaster')
+
 @section('title')
 <title>Edit Firestring</title>
 @stop
 
 @section('editfirestring')
-	
-	@foreach($errors->all() as $message) 
-    <div class='error'>{{ $message }}</div>
-	@endforeach
 
- 	<br>
+<br><br>
+<h3>Edit a String of Fire</h3>
+
+<form action="{{ url('/editfirestring/'.$firestring->id) }}" method="post" role="form">
+    @csrf
+    <input type="hidden" name="id" value="{{ $firestring->id }}">
+
+    <div class="row">
+        <div class="col-md-3">
+            <h4>String Info</h4>
+
+            <div class="form-group"><label>Firestring Number</label><input class="form-control" type="text" name="fire_string_number" value="{{ $firestring->fire_string_number }}"></div>
+            <div class="form-group"><label>Distance</label><input class="form-control" type="text" name="distance" value="{{ $firestring->distance }}"></div>
+            <div class="form-group"><label>Target Number</label><input class="form-control" type="text" name="target" value="{{ $firestring->target }}"></div>
+            <div class="form-group"><label>Relay</label><input class="form-control" type="text" name="relay" value="{{ $firestring->relay }}"></div>
+            <div class="form-group"><label>Light Direction</label><input class="form-control" type="text" name="lightdirection" value="{{ $firestring->lightdirection }}"></div>
+            <div class="form-group"><label>Wind Direction</label><input class="form-control" type="text" name="winddirection" value="{{ $firestring->winddirection }}"></div>
+            <div class="form-group"><label>Wind Speed</label><input class="form-control" type="text" name="windspeed" value="{{ $firestring->windspeed }}"></div>
+            <div class="form-group"><label>Elevation</label><input class="form-control" type="text" name="elevation" value="{{ $firestring->elevation }}"></div>
+            <div class="form-group"><label>Windage</label><input class="form-control" type="text" name="windage" value="{{ $firestring->windage }}"></div>
+        </div>
+
+        <div class="col-md-3">
+            <h4>Shots</h4>
+
+            @for ($i = 1; $i <= 10; $i++)
+                <div class="form-group">
+                    <label>Shot {{ $i }}</label>
+                    <input class="form-control shot-score" type="text" name="shot{{ $i }}value" value="{{ $firestring->{'shot'.$i.'value'} }}">
+                    <input type="hidden" id="shot{{ $i }}x" name="shot{{ $i }}x" value="{{ $firestring->{'shot'.$i.'x'} }}">
+                    <input type="hidden" id="shot{{ $i }}y" name="shot{{ $i }}y" value="{{ $firestring->{'shot'.$i.'y'} }}">
+                    <small id="shot{{ $i }}coords" class="text-muted"></small>
+                </div>
+            @endfor
+        </div>
+
+        <div class="col-md-6">
+            <h4>Plot Shots</h4>
+            <p class="text-muted">Select a shot number, then click the target to place it.</p>
+
+            <div class="form-group">
+                <label for="activeShot">Active Shot</label>
+                <select id="activeShot" class="form-control" style="max-width:180px;">
+                    @for ($i = 1; $i <= 10; $i++)
+                        <option value="{{ $i }}">Shot {{ $i }}</option>
+                    @endfor
+                </select>
+            </div>
+
+            <canvas id="targetCanvas" width="550" height="550" style="border:1px solid #ccc; max-width:100%; cursor:crosshair;"></canvas>
+
+            <br><br>
+            <button type="button" id="clearActiveShot" class="btn btn-warning">Clear Active Shot</button>
+            <button type="button" id="clearAllShots" class="btn btn-danger">Clear All Shots</button>
+        </div>
+    </div>
+
     <br>
-    <!-- Main page section for stock edit -->
-        <h3>Edit a String of Fire</h3>
+    <input type="submit" value="Save" class="btn btn-primary">
+    <a href="/indexfirestring/{{ $firestring->match_id }}" class="btn btn-default">Cancel</a>
+</form>
 
-        <div class = "row">
-	    	<div class = "col-md-4"> 
-        
-	        	<form action="{{ url('/editfirestring/'.$firestring->id) }}" method="post" role="form">
-				
-					<input type="hidden" name="id" value="{{ $firestring->id }}" />
-					
-					<div class="form-group">
-						<input type="text" class="form-group" name="fire_string_number" value="{{ $firestring->fire_string_number }}" />
-						<label for="fire_string_number">Firestring Number</label>
-					</div>
-					<div class="form-group">
-						<input type="select" class="form-group" name="distance" value="{{ $firestring->distance }}" />
-						<label for="distance">Distance</label>
-						
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="target" value="{{ $firestring->target }}" />
-						<label for="target">Target Number</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="relay" value="{{ $firestring->relay }}" />
-						<label for="relay">Relay</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="lightdirection" value="{{ $firestring->lightdirection }}" />
-						<label for="lightdirection">Light Direction</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="winddirection" value="{{ $firestring->winddirection }}" />
-						<label for="winddirection">Wind Direction</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="windspeed" value="{{ $firestring->windspeed }}" />
-						<label for="windspeed">Wind Speed</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="elevation" value="{{ $firestring->elevation }}" />
-						<label for="elevation">Elevation</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="windage" value="{{ $firestring->windage }}" />
-						<label for="windage">Windage</label>
-					</div>
-			</div>
-			<div class = "col-md-4"> 
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot1value" value="{{ $firestring->shot1value }}" />
-						<label for="shot1value">Shot 1</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot2value" value="{{ $firestring->shot2value }}" />
-						<label for="shot2value">Shot 2</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot3value" value="{{ $firestring->shot3value }}" />
-						<label for="shot3value">Shot 3</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot4value" value="{{ $firestring->shot4value }}" />
-						<label for="shot4value">Shot 4</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot5value" value="{{ $firestring->shot5value }}" />
-						<label for="shot5value">Shot 5</label>
-					</div>
-			</div>
-			<div class = "col-md-4"> 
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot6value" value="{{ $firestring->shot6value }}" />
-						<label for="shot6value">Shot 6</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot7value" value="{{ $firestring->shot7value }}" />
-						<label for="shot7value">Shot 7</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot8value" value="{{ $firestring->shot8value }}" />
-						<label for="shot8value">Shot 8</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot9value" value="{{ $firestring->shot9value }}" />
-						<label for="shot9value">Shot 9</label>
-					</div>
-					<div class="form-group">
-						<input type="text" class="form-group" name="shot10value" value="{{ $firestring->shot10value }}" />
-						<label for="shot10value">Shot 10</label>
-					</div>
-			</div>
-					
-				
-				<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-				<input type="submit"value="Save" class="btn btn-primary" />
+<script>
+(function () {
+    const canvas = document.getElementById('targetCanvas');
+    const ctx = canvas.getContext('2d');
+    const activeShotSelect = document.getElementById('activeShot');
 
-				<a href="/indexfirestring/{{ $firestring->match_id }}" class="btn btn-default">Cancel</a>
-			
-			</form>
-		</div>
-            
-           
-                        
+    const center = 275;
+    const maxRadius = 250;
+
+    const shots = {};
+
+    for (let i = 1; i <= 10; i++) {
+        const xInput = document.getElementById(`shot${i}x`);
+        const yInput = document.getElementById(`shot${i}y`);
+
+        shots[i] = {
+            x: xInput.value === '' ? null : Number(xInput.value),
+            y: yInput.value === '' ? null : Number(yInput.value)
+        };
+    }
+
+    function drawTarget() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#f9f9f9';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 10; i >= 1; i--) {
+            const r = maxRadius * i / 10;
+
+            ctx.beginPath();
+            ctx.arc(center, center, r, 0, Math.PI * 2);
+            ctx.fillStyle = i <= 4 ? '#222' : '#fff';
+            ctx.fill();
+            ctx.strokeStyle = '#333';
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(center - maxRadius, center);
+        ctx.lineTo(center + maxRadius, center);
+        ctx.moveTo(center, center - maxRadius);
+        ctx.lineTo(center, center + maxRadius);
+        ctx.strokeStyle = '#999';
+        ctx.stroke();
+
+        ctx.fillStyle = '#000';
+        ctx.font = '13px Arial';
+        ctx.fillText('200 Yard Target', 12, 22);
+    }
+
+    function drawShots() {
+        Object.keys(shots).forEach(function (key) {
+            const shot = shots[key];
+
+            if (shot.x === null || shot.y === null || Number.isNaN(shot.x) || Number.isNaN(shot.y)) {
+                return;
+            }
+
+            const isInBlack = Math.hypot(shot.x - center, shot.y - center) <= (maxRadius * 4 / 10);
+
+            ctx.beginPath();
+            ctx.arc(shot.x, shot.y, 9, 0, Math.PI * 2);
+            ctx.fillStyle = isInBlack ? '#ffffff' : '#d9534f';
+            ctx.fill();
+            ctx.strokeStyle = '#000000';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.fillStyle = isInBlack ? '#000000' : '#ffffff';
+            ctx.font = 'bold 13px Arial';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText(key, shot.x, shot.y);
+        });
+    }
+
+    function updateInputs() {
+        for (let i = 1; i <= 10; i++) {
+            const xInput = document.getElementById(`shot${i}x`);
+            const yInput = document.getElementById(`shot${i}y`);
+            const coords = document.getElementById(`shot${i}coords`);
+
+            if (shots[i].x === null || shots[i].y === null) {
+                xInput.value = '';
+                yInput.value = '';
+                coords.textContent = '';
+            } else {
+                xInput.value = Math.round(shots[i].x);
+                yInput.value = Math.round(shots[i].y);
+                coords.textContent = `x: ${xInput.value}, y: ${yInput.value}`;
+            }
+        }
+    }
+
+    function redraw() {
+        drawTarget();
+        drawShots();
+        updateInputs();
+    }
+
+    canvas.addEventListener('click', function (event) {
+        const rect = canvas.getBoundingClientRect();
+        const scaleX = canvas.width / rect.width;
+        const scaleY = canvas.height / rect.height;
+
+        const x = (event.clientX - rect.left) * scaleX;
+        const y = (event.clientY - rect.top) * scaleY;
+
+        const activeShot = Number(activeShotSelect.value);
+
+        shots[activeShot] = { x, y };
+
+        if (activeShot < 10) {
+            activeShotSelect.value = activeShot + 1;
+        }
+
+        redraw();
+    });
+
+    document.getElementById('clearActiveShot').addEventListener('click', function () {
+        const activeShot = Number(activeShotSelect.value);
+        shots[activeShot] = { x: null, y: null };
+        redraw();
+    });
+
+    document.getElementById('clearAllShots').addEventListener('click', function () {
+        for (let i = 1; i <= 10; i++) {
+            shots[i] = { x: null, y: null };
+        }
+        redraw();
+    });
+
+    redraw();
+})();
+</script>
+
 @stop
-
-
